ca284accc229e81f7cb52898733d66bd1a90c1cf
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-21 23:18:46 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    //int a= 14;
+    //int b= 1218;
+    int a=14,b=15;
+    float pi=3.14;
+
+    char c ='A' ;      
+    cout<<"This is tutorial.\nHere the value of a is "<<a<<". \nThe value of b is "<<b;
+
+    cout<<"\nThe value of pi is: "<<pi;
+    cout<<"\nThe value of c  is:"<<c;
+    return 0;
+}
