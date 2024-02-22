0e35683ec13fa72958fe2989b366e8abefc4b590
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-22 23:32:21 +0530

    Add files via upload

---
+++ mult.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    float num3;
+    cout<<"Enter the first number:";
+    cin>>num1;
+    cout<<"Enter the second number:";
+    cin>>num2;
+    cout<<"Enter the third  number:";
+    cin>>num3;
+    cout<<"The multiplication is:"<<num1*num2*num3;
+    return 0;
+}
