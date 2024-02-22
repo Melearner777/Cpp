106ee31fd1cc6fb968ba66b72c42a665716bb15e
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-22 23:32:56 +0530

    Add files via upload

---
+++ subtraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int first_number, second_number;
+
+    cout<<"Enter the first number:";
+    cin>>first_number;
+    cout<<"Enter the second number:";
+    cin>>second_number;
+    cout<<"The subtraction is :"<<first_number - second_number;
+
+    return 0;
+
+}
