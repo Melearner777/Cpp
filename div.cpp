7b0dee9836691b3bb72206b8b5b3d7571ec23974
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-22 23:34:03 +0530

    Add files via upload

---
+++ div.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using  namespace std;
+int main(){
+    int num1,num2;
+    cout<<"Enter the first number:";
+    cin>>num1;
+    cout<<"Enter the second number:";
+    cin>>num2;
+    cout<<"Division of two number:"<<num1/num2;
+    return 0;
+}
