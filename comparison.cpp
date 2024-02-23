979cbbe7bf35b23db4fa696c049f5ee78478796e
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-23 18:31:13 +0530

    Add files via upload

---
+++ comparison.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=7;
+    cout<<"This is comparision operator"<<endl;
+    cout<<"The value of a==b is "<<(a==b)<<endl;
+    cout<<"The value of a!=b is "<<(a!=b)<<endl;
+    cout<<"The value of a>=b is "<<(a>=b)<<endl;
+    cout<<"The value of a<=b is "<<(a<=b)<<endl;
+    cout<<"The value of a>b is "<<(a>b)<<endl;
+    cout<<"The value of a<b is "<<(a<b)<<endl;
+
+    return 0;
+}
