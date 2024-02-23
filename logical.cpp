3899746f9fede58c4c3a83121103ca7f59e63604
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-23 18:31:23 +0530

    Add files via upload

---
+++ logical.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3,b=7;
+    cout<<"This is are the type of logical operator"<<endl;
+    cout<<"The value of this logical and operator ((a==b)&&(a<b)) is: "<<((a==b)&&(a<b))<<endl;
+    cout<<"The value of this logical or operator ((a==b)or(a>b)) is: "<<((a==b)||(a>b))<<endl;
+    cout<<"The value of this logical not operator (a!=b) is: "<<(a!=b)<<endl;
+
+
+    return 0;
+}
