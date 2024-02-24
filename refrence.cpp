d5e6626c31b0625cd4b9367c853a7ec049ae2150
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-24 18:47:14 +0530

    Add files via upload

---
+++ refrence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z,m;
+    x=144;
+    cout<<"This is the value of x:"<<x<<endl;
+    y=x;
+    cout<<"This is the value of y:"<<y<<endl;
+    cout<<"Enter the value of z:";
+    cin>>z;
+    m=z+x+y;
+    cout<<"This is the value of m:"<<m;
+
+    return 0;
+
+}
