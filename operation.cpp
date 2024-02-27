74f65a1bfb105b34a26ac832b78beba569a9e7f9
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-27 23:25:57 +0530

    Add files via upload

---
+++ operation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3,b=54;
+    int c=(a*5)+b-45+87;
+    int d=((((a*5)+b)-45))+85;
+
+
+    cout<<endl<<c;
+    cout<<endl<<d;
+    return 0;
+}
