08523260d25e4c49d42858bddb67ae66cb8aeae0
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-02-27 23:26:22 +0530

    Add files via upload

---
+++ constant2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3,b=45,c=5145;
+    cout<<"The value of a is :"<<a<<endl;
+    cout<<"The value of b is :"<<b<<endl;
+    cout<<"The value of c is :"<<c<<endl;
+
+    return 0;
+}
